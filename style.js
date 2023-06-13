709ad97e12388327af279c96d8eca74eb3a24426
Excellent Ayomide <excellentayomide@gmail.com>
2023-06-13 15:21:46 +0100

    original commit

---
+++ style.js
@@ -0,0 +1,33 @@
+
+var vid1 = '<iframe class="replace" src="https://www.youtube.com/embed/mkbxP0rxt6E" title="Highlands (Song Of Ascent) [Live] Hillsong UNITED" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+
+var vid2 = '<iframe class="replace" src="https://www.youtube.com/embed/59-PQeL8E08" title="Love So Great - Hillsong Worship" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+
+
+var vid3 = '<iframe class="replace" src="https://www.youtube.com/embed/NS3OgXaHoyc" title="Grace To Grace - Hillsong Worship" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+
+var vid4 = '<iframe class="replace" src="https://www.youtube.com/embed/z9UB5U7kSec" title="Behold (Then Sings My Soul) - Hillsong Worship" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+
+var vid5 = '<iframe class="replace" src="https://www.youtube.com/embed/GfVd5x9W1Xc" title="So Will I (100 Billion X) - Hillsong Worship" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+
+
+
+function showVid1(){
+    document.getElementById("l").innerHTML = vid1;
+}
+
+function showVid2(){
+    document.getElementById("l").innerHTML = vid2;
+}
+
+function showVid3(){
+    document.getElementById("l").innerHTML = vid3;
+}
+
+function showVid4(){
+    document.getElementById("l").innerHTML = vid4;
+}
+
+function showVid5(){
+    document.getElementById("l").innerHTML = vid5;
+}
